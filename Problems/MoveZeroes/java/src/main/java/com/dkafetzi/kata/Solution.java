1edd9d0d2a29b0fb501db0d083cacb6451298205
Dimitris Kafetzis <kafetzis.dimitris.andreas@gmail.com>
2021-09-03 17:54:21 +0800

    LeetCode Move Zeroes

---
+++ Problems/MoveZeroes/java/src/main/java/com/dkafetzi/kata/Solution.java
@@ -0,0 +1,35 @@
+package com.dkafetzi.kata;
+
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/*
+ * https://leetcode.com/problems/positions-of-large-groups/
+ *
+ * See README.md for more information
+ *
+ */
+
+class Solution {
+
+	private final static Logger LOGGER = LoggerFactory.getLogger(Solution.class);
+
+    public void moveZeroes(int[] nums) {
+    	if (nums.length <= 1)
+    		return;
+    	
+    	int countZeros = 0;
+    	
+    	for (int i = 0; i < nums.length; i++) {
+    		if (nums[i] == 0)
+    			countZeros++;
+    		else
+    			nums[i - countZeros] = nums[i];
+    	}
+    	
+    	for (int i = nums.length - countZeros; i < nums.length; i++)
+    		nums[i] = 0;
+    }
+}
