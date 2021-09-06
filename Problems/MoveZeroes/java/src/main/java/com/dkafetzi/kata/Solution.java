c8bf779f8a48c80afa6d9f34f92a0e0870491394
Dimitris Kafetzis <kafetzis.dimitris.andreas@gmail.com>
2021-09-06 21:02:48 +0800

    Removing wrong comments

--- Problems/MoveZeroes/java/src/main/java/com/dkafetzi/kata/Solution.java
+++ Problems/MoveZeroes/java/src/main/java/com/dkafetzi/kata/Solution.java
@@ -5,13 +5,6 @@ import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/*
- * https://leetcode.com/problems/positions-of-large-groups/
- *
- * See README.md for more information
- *
- */
-
 class Solution {
 
 	private final static Logger LOGGER = LoggerFactory.getLogger(Solution.class);
@@ -19,16 +12,16 @@ class Solution {
     public void moveZeroes(int[] nums) {
     	if (nums.length <= 1)
     		return;
-    	
+
     	int countZeros = 0;
-    	
+
     	for (int i = 0; i < nums.length; i++) {
     		if (nums[i] == 0)
     			countZeros++;
     		else
     			nums[i - countZeros] = nums[i];
     	}
-    	
+
     	for (int i = nums.length - countZeros; i < nums.length; i++)
     		nums[i] = 0;
     }
--- Problems/ReverseString/java/src/main/java/com/dkafetzi/kata/Solution.java
+++ Problems/ReverseString/java/src/main/java/com/dkafetzi/kata/Solution.java
@@ -3,25 +3,18 @@ package com.dkafetzi.kata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/*
- * https://leetcode.com/problems/positions-of-large-groups/
- *
- * See README.md for more information
- *
- */
-
 class Solution {
 
 	private final static Logger LOGGER = LoggerFactory.getLogger(Solution.class);
 
 	public void reverseStringOn(char[] s) {
 		char[] result = s.clone();
-		
+
 		for (int i = 0, j = s.length - 1; i < s.length && j >= 0; i++, j--) {
 			s[i] = result[j];
 		}
     }
-	
+
 	public void reverseStringO1(char[] s) {
 		for (int i = 0; i < s.length / 2; i++) {
 			char tmp = s[i];
